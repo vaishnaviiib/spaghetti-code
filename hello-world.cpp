6a1a03cf0691b1f8fb6a8c91133c0bc7a7edf710
vaishnaviiib <vaishnaviboppana09@gmail.com>
2024-09-18 19:12:42 -0500

    Update hello-world.cpp

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     
     // my comment
-    std::cout << "Hello UTD" << std::endl;
 
+    string words[2] = new string ["Hello", "World"];
+    for (int i = 0; i < words.length; i++)
+    {
+        cout << words[i];
+        cout << " ";
+    }
+    cout << endl;
     return 0;
 }
